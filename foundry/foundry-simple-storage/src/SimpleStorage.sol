4b673e0b0123dcf4dc9ef0dc9c9634ef44c94a05
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-02-23 12:24:49 +0600

    damn, learned a lot about handling private keys + now I can use foundry script
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-simple-storage/src/SimpleStorage.sol
+++ foundry/foundry-simple-storage/src/SimpleStorage.sol
@@ -1,13 +1,31 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
 
 contract SimpleStorage {
-    uint256 public data;
+    uint256 public myFavNum;
 
-    function set(uint256 _data) public {
-        data = _data;
+    struct Person {
+        string name;
+        uint8 age;
+        uint256 favNum;
+    }
+
+    mapping(string => Person) nameToPersonDetails;
+
+    function storeMyFavNum(uint256 _myFavNum) public {
+        myFavNum = _myFavNum;
+    }
+
+    function retrieveMyFavNum() public view returns (uint256) {
+        return myFavNum;
+    }
+
+    function storePersonDetails(
+        string memory _name,
+        uint8 _age,
+        uint256 _favNum
+    ) public {
+        nameToPersonDetails[_name] = Person(_name, _age, _favNum);
     }
 }
-
-// SPDX-License-Identifier: MIT
