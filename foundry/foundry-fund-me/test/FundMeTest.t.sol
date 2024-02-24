f43d151a47880a4017069a0c097cb7367c77d449
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-02-24 22:53:50 +0600

    some comments removed
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry/foundry-fund-me/test/FundMeTest.t.sol
@@ -26,27 +26,6 @@ contract FundMeTest is Test {
         assertEq(fundMe.i_fundOwner(), address(this));
     }
 
-    /* 
-        So our test is reverting. What can we do to work with addresses outside our system?
-        1. Unit testing
-            - testing a specific part of our code
-        2. Integration testing
-            - testing how our code works with other parts of our code
-        3. Forked
-            - testing our code on a simulated real environment
-        4. Staging
-            - testing our code in a real environment, like testnet or sometimes even in mainnet
-            - this is highly important for smart contracts because our real env might be very different from our simulated env
-        
-        Till now we have done Unit testing, now we will do Integration testing + Forked testing
-        To do that we will use our Sepolia RPC URL from Alchemy, and run
-
-        forge test --mt testPriceFeedVersionIsAccurate -vvv --fork-url $SEPOLIA_RPC_URL
-        OR
-        forge test -vvv --fork-url $SEPOLIA_RPC_URL
-
-        Now we will see our test will pass
-    */ 
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
