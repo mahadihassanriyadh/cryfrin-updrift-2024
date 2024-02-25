c43ee481822563a6f8da1b2e64d825111fc64932
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-02-25 21:19:42 +0600

    test and deployed script improved
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry/foundry-fund-me/test/FundMeTest.t.sol
@@ -5,25 +5,23 @@ pragma solidity ^0.8.24;
 // import {Test, console} from "forge-std/Test.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinUsdIsFive() public {
         assertEq(fundMe.MIN_USD(), 5e18);
     }
 
-    /* 
-        - here FundMeTest is the owner of the FundMe contract as it created it
-        - so if we would call msg.sender it would be the sender of the transaction, not the owner of the contract
-        - but address(this) gives us the address of our test contract
-    */
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_fundOwner(), address(this));
+        // assertEq(fundMe.i_fundOwner(), address(this));
+        assertEq(fundMe.i_fundOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
