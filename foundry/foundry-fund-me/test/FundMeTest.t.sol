c03a51afaf1f356052f1d6dca223dc8c7175a2c1
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-02-28 19:27:29 +0600

    testWithdrawFromMultipleFunders done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-fund-me/test/FundMeTest.t.sol
+++ foundry/foundry-fund-me/test/FundMeTest.t.sol
@@ -11,7 +11,7 @@ contract FundMeTest is Test {
     FundMe fundMe;
 
     address USER = makeAddr("User");
-    uint256 constant SEND_VALUE = 0.1 ether; // 1e17 wei
+    uint256 constant SEND_VALUE = 5 ether; // 1e17 wei
     uint256 constant STARTING_BALANCE = 8e18; // 8 ether
 
     function setUp() external {
@@ -77,8 +77,53 @@ contract FundMeTest is Test {
         // Assert: And finally assert the test
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
         uint256 endingContractBalance = address(fundMe).balance;
-        
-        assertEq(endingOwnerBalance, startingOwnerBalance + startingContractBalance);
+
+        assertEq(
+            endingOwnerBalance,
+            startingOwnerBalance + startingContractBalance
+        );
         assertEq(endingContractBalance, 0);
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        // Arrange
+        uint160 numOfFunders = 10;
+        uint160 startingFunderIdx = 1;
+        // The reason we are starting from 1 is, because sometimes the address(0) will reverse and won't let us do stuff with it
+        for (uint160 i = startingFunderIdx; i < numOfFunders; i++) {
+            /* 
+                We could do something like this:
+                1. vm.prank(USER); // create a new user
+                2. vm.deal(USER, 1e18); // give the user some funds
+
+                However, foundry gives us another cheatcode `hoax` which is a combination of `prank` and `deal`
+                So we can do this instead:
+                    hoax(address, value)
+            */
+            // previous we have seen we can create an address with address(0)
+            // we can do this with other numbers as well, just the number has to be in uint160 as uint160 has the same byte size as address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingContractBalance = address(fundMe).balance;
+
+        // Act
+        /* 
+            vm.prank(fundMe.getOwner());
+            fundMe.withdraw(); 
+        */
+        // We can do the above two lines, but we can also do this:
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();       
+
+        // Assert
+        assertEq(
+            fundMe.getOwner().balance,
+            startingOwnerBalance + startingContractBalance
+        );
+        assertEq(address(fundMe).balance, 0);
     }
 }
