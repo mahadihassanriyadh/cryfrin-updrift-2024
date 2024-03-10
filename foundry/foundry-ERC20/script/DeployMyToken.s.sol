9674a22e595e97430fcee9d813affd819c8542e1
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-03-10 17:22:43 +0600

    successfully deployed MyToken in local Anvil Chain
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-ERC20/script/DeployMyToken.s.sol
+++ foundry/foundry-ERC20/script/DeployMyToken.s.sol
@@ -3,5 +3,14 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {MyToken} from "../src/MyToken.sol";
 
-contract DeployMyToken is Script {}
+contract DeployMyToken is Script {
+    uint256 public constant INITIAL_SUPPLY = 1000 ether;
+
+    function run() external {
+        vm.startBroadcast();
+        new MyToken(INITIAL_SUPPLY);
+        vm.stopBroadcast();
+    }
+}
