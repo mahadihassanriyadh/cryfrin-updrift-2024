578230a53df854ec6ed7416ba82c6e7de433607d
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-03-10 16:41:54 +0600

    creating my first token with OpenZepplin
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-ERC20/src/ManualToken.sol
+++ foundry/foundry-ERC20/src/ManualToken.sol
@@ -3,6 +3,8 @@
 pragma solidity ^0.8.24;
 
 contract ManualToken {
+    mapping(address => uint256) private s_addressToBalance;
+
     function name() public pure returns (string memory) {
         return "Manual Token";
     }
@@ -15,8 +17,23 @@ contract ManualToken {
         return 18;
     }
 
-    function totalSupply() public pure returns (uint256)
-    {
+    function totalSupply() public pure returns (uint256) {
         return 100 ether;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return s_addressToBalance[_owner];
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(s_addressToBalance[msg.sender] >= _value);
+        uint256 previousBalances = s_addressToBalance[msg.sender] + s_addressToBalance[_to];
+        s_addressToBalance[msg.sender] -= _value;
+        s_addressToBalance[_to] += _value;
+        require(s_addressToBalance[msg.sender] + s_addressToBalance[_to] == previousBalances);
+        return true;
     }
 }
