069896ee898407243e3fcc636eb5c44cd60247ce
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-03-11 19:41:04 +0600

    basic project structure done
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-first-nft-project/src/BasicNFT.sol
+++ foundry/foundry-first-nft-project/src/BasicNFT.sol
@@ -2,8 +2,21 @@
 
 pragma solidity ^0.8.24;
 
-contract BasicNFT {
-    constructor() {
-        
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+    /*  
+        - Each NFT has a unique identifier, which is the token ID.
+        - The combination of the contract address and the token ID is a unique identifier for each NFT.
+        - For us, we are goin to have a token counter representing each token ID.
+    */
+
+    uint256 private s_tokenCounter;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        s_tokenCounter = 0;
     }
 }
