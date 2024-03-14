fe96e7ba4da89af9198b874706dbec487f910bae
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-03-14 06:14:54 +0600

    some refactoring
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-first-nft-project/script/Interactions.s.sol
+++ foundry/foundry-first-nft-project/script/Interactions.s.sol
@@ -7,10 +7,8 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNFT} from "../src/BasicNFT.sol";
 
 contract MintBasicNFT is Script {
-    string public constant NIPPY1 =
+    string public constant NIPPY =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
-    string public constant NIPPY2 =
-        "ipfs://QmQKfTbGSN1XCYG6djwc4LNq8op5zbVxGzv7Vt8nzU7HGr/1792.json";
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
@@ -22,7 +20,7 @@ contract MintBasicNFT is Script {
 
     function mintNFTOnContract(address _contractAddress) public {
         vm.startBroadcast();
-        BasicNFT(_contractAddress).mintNFT(NIPPY1);
+        BasicNFT(_contractAddress).mintNFT(NIPPY);
         vm.stopBroadcast();
     }
 }
--- foundry/foundry-first-nft-project/src/MoodNft.sol
+++ foundry/foundry-first-nft-project/src/MoodNft.sol
@@ -6,18 +6,18 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    string private s_sadSvgImgUri;
+    string private s_happySvgImgUri;
 
     constructor(
         string memory _name,
         string memory _symbol,
-        string memory _sadSvg,
-        string memory _happySvg
+        string memory _sadSvgImgUri,
+        string memory _happySvgImgUri
     ) ERC721(_name, _symbol) {
         s_tokenCounter = 0;
-        s_sadSvg = _sadSvg;
-        s_happySvg = _happySvg;
+        _sadSvgImgUri = _sadSvgImgUri;
+        _happySvgImgUri = _happySvgImgUri;
     }
 
     function mintNft() public {
