5cb66c26400f5a94a8ddf519a070e4c1dd546ebe
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-03-17 23:05:04 +0600

    add more comments to sublesson
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-first-nft-project/src/sublesson/Encoding.sol
+++ foundry/foundry-first-nft-project/src/sublesson/Encoding.sol
@@ -79,4 +79,38 @@ contract Encoding {
         string memory someString = string(multiEncodePacked());
         return someString;
     }
+
+    // As of 0.8.13, you can now do `string.concat(string1, string2)`
+
+    // This abi.encoding stuff seems a little hard just to do string concatenation... is this for anything else?
+    // Why yes, yes it is.
+    // Since we know that our solidity is just going to get compiled down to this binary stuff to send a transaction...
+
+    // We could just use this superpower to send transactions to do EXACTLY what we want them to do...
+
+    // Remeber how before I said you always need two things to call a contract:
+    // 1. ABI
+    // 2. Contract Address?
+    // Well... That was true, but you don't need that massive ABI file. All we need to know is how to create the binary to call
+    // the functions that we want to call.
+
+    // Solidity has some more "low-level" keywords, namely "staticcall" and "call". We've used call in the past, but
+    // haven't really explained what was going on. There is also "send"... but basically forget about send.
+
+    // call: How we call functions to change the state of the blockchain.
+    // staticcall: This is how (at a low level) we do our "view" or "pure" function calls, and potentially don't change the blockchain state.
+
+    // When you call a function, you are secretly calling "call" behind the scenes, with everything compiled down to the binary stuff
+    // for you. Flashback to when we withdrew ETH from our raffle:
+
+    function withdraw(address recentWinner) public {
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        require(success, "Transfer Failed");
+    }
+
+    // Remember this?
+    // - In our {} we were able to pass specific fields of a transaction, like value.
+    // - In our () we were able to pass data in order to call a specific function - but there was no function we wanted to call!
+    // We only sent ETH, so we didn't need to call a function!
+    // If we want to call a function, or send any data, we'd do it in these parathesis!
 }
