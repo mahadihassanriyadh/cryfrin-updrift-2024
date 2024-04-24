32fd1c2c9f791b3f0280f9917454b8c7ecc72a65
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-04-24 22:39:14 +0600

    basic done with HelperConfig
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-defi-stablecoin/script/HelperConfig.s.sol
+++ foundry/foundry-defi-stablecoin/script/HelperConfig.s.sol
@@ -18,11 +18,17 @@ contract HelperConfig is Script {
     uint8 public constant DECIMALS = 8;
     int256 public constant ETH_USD_PRICE = 2500e8;
     int256 public constant BTC_USD_PRICE = 65000e8;
-    int256 public constant DEFAUT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+    uint256 public constant DEFAUT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     NetworkConfig public activeNetworkConfig;
 
-    constructor() {}
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliadEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
 
     function getSepoliadEthConfig() public view returns (NetworkConfig memory) {
         return NetworkConfig({
