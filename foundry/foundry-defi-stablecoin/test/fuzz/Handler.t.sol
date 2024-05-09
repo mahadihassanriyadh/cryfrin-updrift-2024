ec9756c68d1eaef21bb7ff730328d97ecf9909e4
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-09 12:35:57 +0600

    implementing invariant testing for our contract
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

---
+++ foundry/foundry-defi-stablecoin/test/fuzz/Handler.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+/*  
+    [ANSI Regular](https://patorjk.com/software/taag/#p=display&f=ANSI%20Regular&t=Handler)
+    ██   ██  █████  ███    ██ ██████  ██      ███████ ██████  
+    ██   ██ ██   ██ ████   ██ ██   ██ ██      ██      ██   ██ 
+    ███████ ███████ ██ ██  ██ ██   ██ ██      █████   ██████  
+    ██   ██ ██   ██ ██  ██ ██ ██   ██ ██      ██      ██   ██ 
+    ██   ██ ██   ██ ██   ████ ██████  ███████ ███████ ██   ██ 
+
+    ⭐️ This file is used for handler based testing in foundry.
+    In foundry there are two types of Invariant testing:
+        - Open Testing
+        - Handler-Based Testing (This is more helpful to test our invariants in a more controlled environment, which increases the chances of finding bugs in our invariants.)
+*/
+
+pragma solidity ^0.8.24;
