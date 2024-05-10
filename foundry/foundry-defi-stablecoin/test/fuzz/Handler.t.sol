6d4a4f77c6042d03742f025d0c8c1935e9b113d3
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-10 11:16:38 +0600

    yayy, first handler done depositCollateral
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-defi-stablecoin/test/fuzz/Handler.t.sol
+++ foundry/foundry-defi-stablecoin/test/fuzz/Handler.t.sol
@@ -18,19 +18,36 @@ pragma solidity ^0.8.24;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract Handler is Test {
     DSCEngine engine;
     DecentralizedStableCoin dsc;
 
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 constant MAX_DEPOSIT_SIZE = type(uint96).max; // max value for uint96
+
     constructor(DSCEngine _engine, DecentralizedStableCoin _dsc) {
         engine = _engine;
         dsc = _dsc;
+
+        address[] memory collateralTokens = engine.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
     }
 
     // don't call redeemCollateral if there is no collateral
-    function depositCollateral(address _collateralSeed, uint256 _amountCollateral) public {
-        engine.depositCollateral(_collateralSeed, _amountCollateral);
+    function depositCollateral(uint256 _collateralSeed, uint256 _amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(_collateralSeed);
+        _amountCollateral = bound(_amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, _amountCollateral);
+        collateral.approve(address(engine), _amountCollateral);
+        engine.depositCollateral(address(collateral), _amountCollateral);
+        vm.stopPrank();
     }
 
     /*  
@@ -38,4 +55,10 @@ contract Handler is Test {
         ######## Helper Functions ########
         ##################################
     */
+    function _getCollateralFromSeed(uint256 _collateralSeed) private view returns (ERC20Mock) {
+        if (_collateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
+    }
 }
