315c9f75db45c07078f11a4c6227371f14277ac1
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-14 14:24:18 +0600

    wow, UUPS are crazy stuffs
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-upgradable-contracts/src/BoxV1.sol
+++ foundry/foundry-upgradable-contracts/src/BoxV1.sol
@@ -14,4 +14,14 @@ contract BoxV1 is UUPSUpgradeable {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    /** 
+     * @dev this function checks if the caller is authorized to upgrade the contract
+     * this is where we usually implement those, onlyOwner or onlyAdmin checks
+     * if we want this to be authorized by a DAO we can implement a modifier that checks if the caller is a DAO
+     * 
+     * @notice but for now we don't really care, anyone can upgrade this. So we will just leave it blank for now.
+     * 
+    */
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
