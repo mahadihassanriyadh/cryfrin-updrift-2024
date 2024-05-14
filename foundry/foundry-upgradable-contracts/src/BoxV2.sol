ee286174e09f4b9bc32647597d1e059653f00257
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-14 16:37:48 +0600

    updated BoxV2 as well
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-upgradable-contracts/src/BoxV2.sol
+++ foundry/foundry-upgradable-contracts/src/BoxV2.sol
@@ -3,12 +3,20 @@
 pragma solidity ^0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is UUPSUpgradeable {
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {
-        number = _number;
+    // @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
     }
 
     function getNumber() external view returns (uint256) {
@@ -18,4 +26,6 @@ contract BoxV2 is UUPSUpgradeable {
     function version() external pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
