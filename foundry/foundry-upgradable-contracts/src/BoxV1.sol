a4bf711f40820ec7752d8b851f9b2c54856ba7e1
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-14 14:41:05 +0600

    understood Storage Gaps, and why it's not being used currently
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-upgradable-contracts/src/BoxV1.sol
+++ foundry/foundry-upgradable-contracts/src/BoxV1.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
@@ -24,4 +25,19 @@ contract BoxV1 is UUPSUpgradeable {
      * 
     */
     function _authorizeUpgrade(address newImplementation) internal override {}
+
+    /** 
+     * @notice also there was a method in the openzeppelin UUPSUpgradeable contract on previous versions known as "Storage Gaps"
+     * this is a reserved space in the contract storage that allows future versions to add new variables without shifting down storage in the inheritance chain.
+     * 
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     * uint256[50] private __gap;
+     * 
+     * @notice But a better way has been implemented in the new versions
+     * Now "Diamond Storage" is being used, instead of "Storage Gaps"
+     * This is the PR that upgraded the storage layout to Diamond Storage: 
+     *      https://github.com/OpenZeppelin/openzeppelin-contracts/issues/2964
+    */
 }
