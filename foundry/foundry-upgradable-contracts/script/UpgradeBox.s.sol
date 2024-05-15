fb623096bb4555361c5972bb9b1d53650b3d660f
Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>
2024-05-15 17:20:47 +0600

    done with the scripts for DeployBox and UpgradeBox
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+mahadihassanriyadh@users.noreply.github.com>

--- foundry/foundry-upgradable-contracts/script/UpgradeBox.s.sol
+++ foundry/foundry-upgradable-contracts/script/UpgradeBox.s.sol
@@ -3,11 +3,30 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {BoxV1} from "../src/BoxV1.sol";
 import {BoxV2} from "../src/BoxV2.sol";
-// import {}
+import {DevOpsTools} from "@foundry-devops/src/DevOpsTools.sol";
 
 contract UpgradeBox is Script {
+    uint256 public constant DEFAUT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     function run() external returns (address) {
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+
+        vm.startBroadcast(DEFAUT_ANVIL_KEY);
+        BoxV2 newBox = new BoxV2(); // new implementation contract (Logic)
+        vm.stopBroadcast();
+
+        address proxy = upgradeBox(mostRecentDeployed, address(newBox));
+        return proxy;
+    }
+
+    function upgradeBox(address _proxyAddress, address _newBox) public returns (address) {
+        vm.startBroadcast(DEFAUT_ANVIL_KEY);
+        BoxV1 proxy = BoxV1(_proxyAddress);
+        proxy.upgradeToAndCall(address(_newBox), abi.encodeWithSignature("initialize()"));
+        vm.stopBroadcast();
         
+        return address(proxy);
     }
 }
