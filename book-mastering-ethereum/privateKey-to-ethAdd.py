f1e454713a173e4d8eb9546c905d9829950cbdff
Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>
2024-08-15 08:49:31 +0600

    trying to get a little hold of cryptography
    Signed-off-by: Md. Mahadi Hassan Riyadh <77486566+0xriyadh@users.noreply.github.com>

--- book-mastering-ethereum/privateKey-to-ethAdd.py
+++ book-mastering-ethereum/privateKey-to-ethAdd.py
@@ -1,4 +1,5 @@
 from ecdsa import SigningKey, SECP256k1
+import sha3
 
 private_key_hex = "f8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315"
 
@@ -19,3 +20,17 @@ y = vk.pubkey.point.y()
 public_key = "04" + format(x, '064x') + format(y, '064x')
 
 print("Public Key:", public_key)
+
+# Serialize the public key in the required format (without the prefix 04)
+public_key_bytes = bytes.fromhex(format(x, '064x') + format(y, '064x'))
+
+# Compute the Keccak-256 hash of the public key
+keccak = sha3.keccak_256()
+keccak.update(public_key_bytes)
+public_key_hash = keccak.hexdigest()
+
+# The Ethereum address is the last 20 bytes of the hash
+eth_address = "0x" + public_key_hash[-40:]
+
+print("Public Key Hash:", public_key_hash)
+print("Ethereum Address:", eth_address)
